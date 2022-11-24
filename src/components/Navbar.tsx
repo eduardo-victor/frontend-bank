9d037b19ff2e61f20185ddb34e1dc0e8300b9abe
eduardo-victor <victorofish@gmail.com>
2022-11-23 23:22:43 -0300

    📦 NEW: Code refactored to TypeScript

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             <h1 className='font-quicksand font-bold text-4xl text-yellow [text-shadow:_0_1px_0_rgb(0_0_0_/_20%)]'>bankey</h1>
         </div>
         <div>
-            <button className='py-2 px-8 mr-8 font-inter border-2 border-yellow bg-yellow rounded-sm hover:bg-transparent hover:border-2 hover:border-yellow hover:text-yellow hover:transition text-white'>Login</button>
+            <button className='py-2 px-8 mr-8 font-inter border-2 border-yellow bg-yellow rounded-sm hover:bg-transparent hover:border-2 hover:border-yellow hover:text-yellow hover:transition text-white'>Login Here!</button>
         </div>
     </div>
   )
