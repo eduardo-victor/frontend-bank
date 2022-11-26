cfced94193fbb94add12280f7df0cbbe546fcbaf
eduardo-victor <victorofish@gmail.com>
2022-11-26 20:48:20 -0300

    👌 IMPROVE: UserPage created

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -2,9 +2,29 @@ import React from 'react'
 import UserNav from '../components/UserNav'
 import Image from '../assets/commentImages/firstComment.png'
 
-const UserPage = () => {
+type GetProps = {
+  image: string,
+  amount: string
+}
+
+
+const UserPage = (props: GetProps) => {
   return (
-    <UserNav image={Image}/>
+    <>
+      <UserNav image={props.image}/>
+      <div className='w-screen h-screen flex flex-col'>
+        <div className='w-screen flex justify-between'>
+          <h1 className='font-inter text-xl mt-7 ml-4'>R${props.amount}</h1>
+          <h1 className='font-inter text-md mt-7 mr-4'>Welcome, {}.</h1>
+        </div>
+      <div className='w-full h-screen flex items-end'>
+        <div className='w-screen h-[90vh] bg-yellow rounded-t-2xl'>
+
+        </div>
+      </div>
+      </div>
+
+    </>
   )
 }
 
