f11d30134f106b799585fdfb8b3b1812eb9a87f2
eduardo-victor <victorofish@gmail.com>
2022-11-26 21:37:47 -0300

    📦 NEW: Create cards for user page.

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type GetProps = {
+    icon: string,
+    title: string,
+    comment : string
+}
+
+const Card = (props: GetProps) => {
+  return (
+    <div className='w-[90%] h-[15vh] drop-shadow-md bg-white rounded-md flex flex-row items-center'>
+        <img src={props.icon} alt=""  className='ml-4'/>
+        <div className='w-full h-full ml-6 mt-4'>
+            <h1 className='text-xl font-inter text-yellow [text-shadow:_0_0.2px_0_rgb(0_0_0_/_10%)]'>{props.title}</h1>
+            <h3 className='font-inter text-[#818181]'>{props.comment}</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Card
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,25 +1,20 @@
 import React from 'react'
 import UserNav from '../components/UserNav'
 import Image from '../assets/commentImages/firstComment.png'
+import Card from '../components/Card'
 
-type GetProps = {
-  image: string,
-  amount: string
-}
-
-
-const UserPage = (props: GetProps) => {
+const UserPage = () => {
   return (
     <>
-      <UserNav image={props.image}/>
+      <UserNav image={'a'}/>
       <div className='w-screen h-screen flex flex-col'>
         <div className='w-screen flex justify-between'>
-          <h1 className='font-inter text-xl mt-7 ml-4'>R${props.amount}</h1>
-          <h1 className='font-inter text-md mt-7 mr-4'>Welcome, {}.</h1>
+          <h1 className='font-inter text-lg mt-7 ml-4'>R$   {'7.895,00'}</h1>
+          <h1 className='font-inter text-lg mt-7 mr-4'>Welcome, {'User'}.</h1>
         </div>
       <div className='w-full h-screen flex items-end'>
-        <div className='w-screen h-[90vh] bg-yellow rounded-t-2xl'>
-
+        <div className='w-screen h-[90vh] bg-yellow rounded-t-2xl flex justify-center items-center'>
+          <Card/>
         </div>
       </div>
       </div>
