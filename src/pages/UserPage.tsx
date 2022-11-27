6d3bbebf1ab9d05c07c8382dc97e3998fcc79196
eduardo-victor <victorofish@gmail.com>
2022-11-26 21:45:59 -0300

    👌 IMPROVE: Added cards to user page

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import UserNav from '../components/UserNav'
 import Image from '../assets/commentImages/firstComment.png'
 import Card from '../components/Card'
+import IconDeposit from '../assets/cardImages/cashpayment.png'
+import IconTransfer from '../assets/cardImages/moneytransf.png'
+import IconCard from '../assets/cardImages/creditcard.png'
+import IconLog from '../assets/cardImages/log.png'
 
 const UserPage = () => {
   return (
@@ -13,8 +17,11 @@ const UserPage = () => {
           <h1 className='font-inter text-lg mt-7 mr-4'>Welcome, {'User'}.</h1>
         </div>
       <div className='w-full h-screen flex items-end'>
-        <div className='w-screen h-[90vh] bg-yellow rounded-t-2xl flex justify-center items-center'>
-          <Card/>
+        <div className='w-screen h-[90vh] bg-yellow rounded-t-2xl flex flex-col gap-8 justify-center items-center'>
+          <Card icon={IconDeposit} title={'Deposit'} comment={'Deposit an amount in your account and start enjoying the benefits of bankey.'}/>
+          <Card icon={IconTransfer} title={'Transfer'} comment={'Transfer any amount to another account in up to three clicks.'}/>
+          <Card icon={IconLog} title={'Statement'} comment={'Check past transactions by clicking here.'}/>
+          <Card icon={IconCard} title={'Card'} comment={'Know the credit requirements and apply for your card here.'}/>
         </div>
       </div>
       </div>
