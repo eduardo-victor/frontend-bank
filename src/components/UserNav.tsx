bcaaeb958ad3a4a7c3f23e32394c6c57ef538f24
eduardo-victor <victorofish@gmail.com>
2022-12-01 10:55:27 -0300

    🐛 FIX:  Correct user page navbar

--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -32,12 +32,12 @@ const UserNav = (props: GetProps) => {
         <div className='w-screen h-[100px] z-50 bg-yellow [text-shadow:_0_1px_0_rgb(0_0_0_/_20%)] md:w-full'>
             <div className='px-4 flex justify-between items-center w-full h-full '>
                 <div className='flex items-center'>
-                    <img src={props.image} alt='Logo' className='w-16 mr-4 lg:w-20'/>
+                    <h1 className='font-quicksand font-bold text-4xl text-white [text-shadow:_0_1px_0_rgb(0_0_0_/_20%)]'>bankey</h1>
                 </div>
-                <div className='hidden md:flex pr-4'>
+                <div className='hidden md:flex pr-4 hover:cursor-pointer'>
                     <img src={Logout} alt="" onClick={logout}/>
                 </div>
-                <div className='md:hidden'>
+                <div className='md:hidden hover:cursor-pointer'>
                     <img src={Logout} alt="" onClick={logout} className='w-10' />
                 </div>
             </div>
